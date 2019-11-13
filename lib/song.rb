e9eac5fc3c9462eda4045440ae4d66942a8ddd58
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-13 15:07:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
          hash_new = Hash.new(0)
          
         @@genres.each do |genre|
-          hash_new[genre]+=1
+          hash_new[@@genres]+=1
            binding.pry
             #'wait'
            
